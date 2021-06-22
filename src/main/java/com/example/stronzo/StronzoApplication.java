098946b21bc907e9a1790d23577ac442c709831e
Philip Atanassov <p.atanassov@ocado.com>
2021-06-23 00:24:15 +0300

    Init

---
+++ src/main/java/com/example/stronzo/StronzoApplication.java
@@ -0,0 +1,13 @@
+package com.example.stronzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StronzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stronzo/StronzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stronzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StronzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
